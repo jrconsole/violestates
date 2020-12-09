42a3ac4a9822cf61ad6865744661e5596837e451
jrconsole <jrconsole@gmail.com>
2020-12-08 22:24:20 -0800

    Add boilerplate express code

---
+++ server/app.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express(); // create an instance of the express module (app is the conventional variable name used)
+
+const PORT = process.env.PORT || 5000; // use either the host env var port (PORT) provided by Heroku or the local port (5000) on your machine
+
+const cors = require('cors'); // import the CORS library to allow Cross-origin resource sharing
+app.use(cors()); // Enable CORS 
+
+var bodyParser = require('body-parser');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(express.json()); // Recognize Request Objects as JSON objects
+app.use(express.static('build')); // serve static files (css & js) from the 'public' directory
+
+
+
+app.listen(PORT, () => {
+    console.log(`Server is listening on port: ${PORT}`)
+})
