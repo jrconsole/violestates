d21830a931db5143aa78aa0fa385bbbe89aabc11
jrconsole <jrconsole@gmail.com>
2021-01-10 17:48:00 -0800

    correct misspelling

--- server/queries.js
+++ server/queries.js
@@ -2,7 +2,7 @@ const Pool = require('pg').Pool;
 const pool = new Pool({
     connectionString: process.env.DATABASE_URL,
     ssl: {
-        rejectUnaithorized: false
+        rejectUnauthorized: false
     }
 });
 const connect = () => { return pool; }
