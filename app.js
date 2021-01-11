cf32f41a0f228297c5d96e36740aa8f1a33df2a1
jrconsole <jrconsole@gmail.com>
2021-01-10 18:50:06 -0800

    move app.js file to support path routing

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express(); // create an instance of the express module (app is the conventional variable name used)
 
-const db = require('./queries');
+const db = require('./server/queries');
 
 const PORT = process.env.PORT || 5000; // use either the host env var port (PORT) provided by Heroku or the local port (5000) on your machine
 
